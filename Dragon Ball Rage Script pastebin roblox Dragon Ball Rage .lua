c8b117333564fe97852d36e102f4f5035a5527e7
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:58:50 +0100

    commit zero

---
+++ Dragon Ball Rage Script pastebin roblox Dragon Ball Rage .lua
@@ -0,0 +1,12 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(("https://maxv.ml/weis/DBRage.lua")))() --By Weismann#8896
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Very bright and juicy script for Dragon Ball Rage is very functional
+and much more
+						
+]]
